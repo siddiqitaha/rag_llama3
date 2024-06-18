15dd9910c4c828a5309cf63e2bfc787201765283
siddiqitaha <92089684+siddiqitaha@users.noreply.github.com>
2024-06-18 11:04:24 +0300

    Create css.py

---
+++ css.py
@@ -0,0 +1,33 @@
+custom_css = """
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+body, button, input, select, textarea {
+    font-family: 'Roboto', sans-serif;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    font-family: 'Roboto', sans-serif;
+    color: #333;
+}
+
+/* Change the background color of the interface */
+body {
+    background-color: #f0f0f0;
+}
+
+/* Change the color and padding of the submit button */
+button {
+    background-color: #1f2945;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+}
+
+/* Change the button on hover */
+button:hover {
+    opacity: 0.8;
+}
+"""
